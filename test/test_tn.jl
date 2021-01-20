884607b52997bbd4a1ed5fab719edaa6a0ecfe53
Niall Moran <niall.moran@ichec.ie>
2021-01-20 14:13:40 +0000

    Add simple contraction function to test output

--- test/test_tn.jl
+++ test/test_tn.jl
@@ -3,16 +3,31 @@ using LightGraphs
 
 @testset "Test circuit to Tensor Network Circuit conversion" begin
     circ = QXSim.create_test_circuit()
-    tnc = convert_to_tnc(circ)
+    tnc = convert_to_tnc(circ, no_input=true, no_output=true)
 
     @test tnc.qubits == 3
+    @test length(tnc.tn.data) == 3
     @test all(tnc.input_indices .!= tnc.output_indices)
+
+    tnc = convert_to_tnc(circ, no_input=false, no_output=true)
+    @test length(tnc.tn.data) == 6
+end
+
+@testset "Test simple circuit contraction to verify convertion to circuit working" begin
+    circ = QXSim.create_test_circuit()
+    # we add input but no output to get full output vector
+    tnc = convert_to_tnc(circ, no_input=false, no_output=true)
+
+    output = QXSim.simple_contraction(tnc.tn)
+    ref = zeros(8)
+    ref[[1,8]] .= 1/sqrt(2)
+    @test all(store(output) .≈ ref)
 end
 
 @testset "Test tensor network to graph conversion" begin
     # prepare circuit and network
     circ = QXSim.create_test_circuit()
-    tnc = convert_to_tnc(circ)
+    tnc = convert_to_tnc(circ, no_input=true, no_output=true)
 
     # convert to graph without inputs or outputs
     g = convert_to_graph(tnc)
